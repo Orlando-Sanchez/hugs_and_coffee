6186008072fbdb92e59ac8b15943d0126001fc65
S662 <or.sanchez100@gmail.com>
2020-06-14 09:38:12 -0400

    Create users signup and signin

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,20 @@
+class ProfileController < ApplicationController
+
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
